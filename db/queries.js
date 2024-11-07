8597f28e24120069cdde1eb44bfa1e9c71d67b71
Tony Huang <170560060+tonyhchii@users.noreply.github.com>
2024-11-07 13:59:34 -0500

    added query to include set name

--- db/queries.js
+++ db/queries.js
@@ -1,6 +1,8 @@
 const pool = require("./pool");
 const getAllCards = async () => {
-  const { rows } = await pool.query("SELECT * FROM cards");
+  const { rows } = await pool.query(
+    "SELECT cards.name as name, cards.url as url, sets.name as setName, rarity, quantity, price, sets.set_id FROM cards JOIN sets ON sets.set_id = cards.set_id"
+  );
   return rows;
 };
 
@@ -8,5 +10,7 @@ const getAllSets = async () => {
   const { rows } = await pool.query("SELECT * FROM sets");
   return rows;
 };
+
+const getCardByName = async () => {};
 
 module.exports = { getAllCards, getAllSets };
